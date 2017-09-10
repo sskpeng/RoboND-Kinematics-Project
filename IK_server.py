153186cc0f730e7c167881092e05de912a844574
Scott Peng <skpeng@MBP.local>
2017-09-10 01:38:48 -0400

    Implemented server by following the debug code

--- IK_server.py
+++ IK_server.py
@@ -28,91 +28,62 @@ def handle_calculate_IK(req):
 		
         ### Your FK code here
         # Create symbols
-	q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6, = symbols('alpha0:7')
-    #
-	#   
-	# Create Modified DH parameters
-    s = {alpha0:     0, a0:      0, d1:  0.75,
-         alpha1: -pi/2, a1:   0.35, d2:     0,  q2: q2-pi/2,
-         alpha2:     0, a2:   1.25, d3:     0,
-         alpha3: -pi/2, a3: -0.054, d4:  1.50,
-         alpha4: -pi/2, a4:      0, d5:     0,
-         alpha5: -pi/2, a5:      0, d6:     0,
-         alpha6:     0, a6:      0, d7: 0.303,  q7: 0 }
-	#
-	#            
-	# Define Modified DH Transformation matrix
-    T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
-                   [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-                   [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-                   [                   0,                   0,            0,               1]])
-    T0_1 = T0_1.subs(s)
+	    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6, = symbols('alpha0:7')
+        #
+	    #   
+	    # Create Modified DH parameters
+        s = {alpha0:     0, a0:      0, d1:  0.75,
+             alpha1: -pi/2, a1:   0.35, d2:     0,  q2: q2-pi/2,
+             alpha2:     0, a2:   1.25, d3:     0,
+             alpha3: -pi/2, a3: -0.054, d4:  1.50,
+             alpha4: -pi/2, a4:      0, d5:     0,
+             alpha5: -pi/2, a5:      0, d6:     0,
+             alpha6:     0, a6:      0, d7: 0.303,  q7: 0 }
+	    #
+	    #            
+	    # Define Modified DH Transformation matrix
+        def TF_Matrix(alpha, a, d, q):
+            TF = Matrix([[          cos(q),           -sin(q),            0,              a],
+                       [ sin(q)*cos(alpha), cos(q)*cos(alpha),  -sin(alpha),  -sin(alpha)*d],
+                       [ sin(q)*sin(alpha), cos(q)*sin(alpha),   cos(alpha),   cos(alpha)*d],
+                       [                 0,                 0,            0,              1]])
+            return TF
+	    #
+	    #
+	    # Create individual transformation matrices
+        T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(s)
+        T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(s)
+        T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(s)
+        T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(s)
+        T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(s)
+        T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(s)
+        T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(s)
 
-    T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
-                   [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-                   [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-                   [                   0,                   0,            0,               1]])
-    T1_2 = T1_2.subs(s)
-
-    T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
-                   [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-                   [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-                   [                   0,                   0,            0,               1]])
-    T2_3 = T2_3.subs(s)
-
-    T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-                   [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-                   [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-                   [                   0,                   0,            0,               1]])
-    T3_4 = T3_4.subs(s)
-
-    T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-                   [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-                   [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-                   [                   0,                   0,            0,               1]])
-    T4_5 = T4_5.subs(s)
-
-    T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-                   [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-                   [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-                   [                   0,                   0,            0,               1]])
-    T5_6 = T5_6.subs(s)
-
-    T6_G = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
-                   [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
-                   [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
-                   [                   0,                   0,            0,               1]])
-    T6_G = T6_G.subs(s)
+        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
 	#
 	#
-	# Create individual transformation matrices
-    T0_2 = simplify(T0_1 * T1_2)
-    T0_3 = simplify(T0_2 * T2_3)
-    T0_4 = simplify(T0_3 * T3_4)
-    T0_5 = simplify(T0_4 * T4_5)
-    T0_6 = simplify(T0_5 * T5_6)
-    T0_G = simplify(T0_6 * T6_G)
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-    R_z = Matrix([[    cos(np.pi), -sin(np.pi),             0, 0],
-                  [    sin(np.pi),  cos(np.pi),             0, 0],
-                  [             0,           0,             1, 0],
-                  [             0,           0,             0, 1]])
-                  
-    R_y = Matrix([[  cos(np.pi/2),           0,  sin(np.pi/2), 0],
-                  [             0,           1,             0, 0],
-                  [ -sin(np.pi/2),           0,  cos(np.pi/2), 0],
-                  [             0,           0,             0, 1]])
+	    # Extract rotation matrices from the transformation matrices
+        R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
+	    #
+	    #
+        r, p, y = symbols('r p y')
 
-    R_corr = simplify(R_z * R_y)
+        ROT_x = Matrix([[       1,            0,         0],
+                        [       0,       cos(r),   -sin(r)],
+                        [       0,       sin(r),    cos(r)]]) # roll
 
-    T_total = simplify(T0_G * R_corr)
-	#
-	#
+        ROT_y = Matrix([[  cos(p),            0,    sin(p)],
+                        [       0,            1,         0],
+                        [ -sin(p),            0,    cos(p)]]) # pitch
+
+        ROT_z = Matrix([[  cos(y),      -sin(y),         0],
+                        [  sin(y),       cos(y),         0],
+                        [       0,            0,         1]]) # yaw
+              
+        ROT_EE = ROT_z * ROT_y * ROT_x
         ###
 
         # Initialize service response
@@ -134,61 +105,43 @@ def handle_calculate_IK(req):
      
             ### Your IK code here 
 	    # Compensate for rotation discrepancy between DH parameters and Gazebo
+            #
+            # Make correction matrix to represent EE orientation in world frame
+            ROT_corr = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+
+            ROT_EE = ROT_EE * ROT_corr
+            ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
+
+            T0_6 = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6
+            R0_6 = T0_6[0:3, 0:3]
+            WC = EE - ROT_EE * Matrix([[0],[0],[0.303]])
+            EE = Matrix([[px], [py], [pz]])
 	    #
 	    #
 	    # Calculate joint angles using Geometric IK method
+            theta1 = atan2(WC[1], WC[0])
 
-        wcx = px - T_total * Matrix([[0],[0],[d7],[0]])[0]
-        wcy = py - T_total * Matrix([[0],[0],[d7],[0]])[1]
-        wcz = pz - T_total * Matrix([[0],[0],[d7],[0]])[2]
-	    #
-        ## z = d1 + a2 * cos(q2) - a3 * cos(q2 + q3) - d4 * sin(q2 + q3)
-        ## l = a1 + a2 * sin(q2) - a3 * sin(q2 + q3) + d4 * cos(q2 + q3)
-        ## x = l * cos(q1)
-        ## y = l * sin(q1)
-        ##
-        ### some trigonometric equations
-        ### sin(a-b) = sin(a) * sin(b) - cos(a) * sin(b)
-        ### cos(a-b) = cos(a) * sin(b) + sin(a) * cos(b)
-        ###
-        ##
-        ## q1 = atan2(wcy, wcx)
-        ## l = wcx / cos(q1)
-        ##
-        ## A = z - d1
-        ## B = a2
-        ## C = a3
-        ## D = d4
-        ## E = l - a1
-        ##
-        ## C * cos(q3) + D * sin(q3) = (B**2 + C**2 + D**2 - A**2 -E**2) / (2 * B)
-        ##
-        ## AA = D
-        ## BB = C
-        ## CC = (B**2 + C**2 + D**2 - A**2 -E**2) / (2 * B)
-        q3 = atan2(AA, BB) - atan2(sqrt(AA**2 + BB**2 - CC**2), CC) # there is another solution with "+"
-        #q3 = atan2(AA, BB) + atan2(sqrt(AA**2 + BB**2 - CC**2), CC)
-        #
-	    #
-        side_a = d4
-        side_b = sqrt((sqrt(wcx**2 + wcy**2) - a1)**2 + (wcz - d1)**2)
-        side_c = a2
+            side_a = 1.501 # d4.subs(s)
+            side_b = sqrt((sqrt(WC[0]**2 + WC[1]**2) - 0.35)**2 + (WC[2] - 0.75)**2) # a1 = 0.35, d1 = 0.75
+            side_c = 1.25 # a2.subs(s)
+    
+            angle_a = acos((side_b**2 + side_c**2 - side_a**2)/(2 * side_b * side_c))
+            angle_b = acos((side_a**2 + side_c**2 - side_b**2)/(2 * side_a * side_c))
+            angle_c = acos((side_a**2 + side_b**2 - side_c**2)/(2 * side_a * side_b))
 
-        angle_a = acos((side_b**2 + side_c**2 - side_a**2)/(2 * side_b * side_c))
-        angle_b = acos((side_a**2 + side_a**2 - side_b**2)/(2 * side_a * side_c))
-        angle_c = acos((side_a**2 + side_b**2 - side_c**2)/(2 * side_a * side_b))
-        
-        theta2 = pi/2 - angle_a - atan2(wcz - d1, sqrt(wcx**2 + wcy**2) - a1)
-        theta3 = pi/2 - (angle_b + 0.036)
+            theta2 = pi/2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0]**2 + WC[1]**2) - 0.35)
+            # angle offset between Jt3 and Jt4 is atan2(a3, d4) = 0.03598...
+            theta3 = pi/2 - (angle_b + 0.036)
 
-        R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
-        R0_3 = R0_3.evalf(subs = {q1: theta1, q2: theta2, q3: theta3})
+            # R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
+            R0_3 = R0_3.evalf(subs = {q1: theta1, q2: theta2, q3: theta3})
 
-        R3_6 = R0_3.inv("LU") * ROT_EE
-
-        theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-        theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2), R3_6[1,2])
-        theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            R3_6 = R0_3.inv("LU") * ROT_EE
+    
+            # Eular angles from rotation matrix
+            theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+            theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
         
             ###
 		
